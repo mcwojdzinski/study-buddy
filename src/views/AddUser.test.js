4c2498f7795871fe69079ddcc81225aa24153ab5
Mateusz Cwojdzinski <45481835+mcwojdzinski@users.noreply.github.com>
2022-05-10 23:14:32 +0200

    Add tests into the project (#7)
    Co-authored-by: Mateusz Cwojdziński <mcwojdzinski@Macbook.local>

---
+++ src/views/AddUser.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import { renderWithProviders } from 'helpers/renderWithThemeProvider';
+import AddUser from './AddUser';
+import Dashboard from './Dashboard';
+
+describe('Input with button', () => {
+  it('Renders the component', () => {
+    renderWithProviders(
+      <>
+        <AddUser />
+        <Dashboard />
+      </>
+    );
+    fireEvent.change(screen.getByTestId('Name'), { target: { value: 'Karol' } });
+    fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
+    fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.5' } });
+    fireEvent.click(screen.getByText('Add'));
+
+    screen.getByText('Karol');
+  });
+});
