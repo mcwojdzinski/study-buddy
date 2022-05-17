592d4d58a783a16908d43790bd9db626fcb43ce9
Mateusz Cwojdzinski <45481835+mcwojdzinski@users.noreply.github.com>
2022-05-17 17:32:29 +0200

    Add news section + fix test and write new one (#9)
    Co-authored-by: Mateusz Cwojdziński <mcwojdzinski@Macbook.local>

--- src/views/AddUser.test.js
+++ src/views/AddUser.test.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { screen, fireEvent } from '@testing-library/react';
-import { renderWithProviders } from 'helpers/renderWithThemeProvider';
 import AddUser from './AddUser';
 import Dashboard from './Dashboard';
+import { screen, fireEvent } from '@testing-library/react';
+import { renderWithProviders } from 'helpers/renderWithThemeProvider';
+import '@testing-library/jest-dom/extend-expect';
 
-describe('Input with button', () => {
-  it('Renders the component', () => {
+describe('Form Field', () => {
+  it('Adds new user to the list', () => {
     renderWithProviders(
       <>
         <AddUser />
         <Dashboard />
       </>
     );
-    fireEvent.change(screen.getByTestId('Name'), { target: { value: 'Karol' } });
+    fireEvent.change(screen.getByTestId('Name'), { target: { value: 'Grażyna' } });
+    fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
+    fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.5' } });
+    fireEvent.click(screen.getByTestId('Consent'));
+    fireEvent.click(screen.getByText('Add'));
+    screen.getByText('Grażyna');
+  });
+
+  it('Prevents adding new user if the consent is not checked', () => {
+    renderWithProviders(
+      <>
+        <AddUser />
+        <Dashboard />
+      </>
+    );
+    fireEvent.change(screen.getByTestId('Name'), { target: { value: 'Grażyna' } });
     fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
     fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.5' } });
     fireEvent.click(screen.getByText('Add'));
-
-    screen.getByText('Karol');
+    const newUser = screen.queryByText('Grażyna');
+    expect(newUser).not.toBeInTheDocument();
   });
 });
