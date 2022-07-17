b24bc41129b7bfd9bb52c2eddf2c8da1de5c3f80
Mateusz Cwojdziński <mcwojdzinski@Macbook.local>
2022-07-17 16:25:21 +0200

    Mocking api + solution with group filtering

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from 'assets/styles/globalStyles';
+import { GlobalStyles } from 'assets/styles/GlobalStyles';
 import { theme } from 'assets/styles/theme';
 import { Wrapper } from './Root.styles';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
-import AddUser from 'views/AddUser';
 import Dashboard from 'views/Dashboard';
-import UsersProvider from 'providers/UsersProvider';
 
 const Root = () => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
+        <GlobalStyles />
         <MainTemplate>
-          <UsersProvider>
-            <Wrapper>
-              <Switch>
-                <Route path="/add-user">
-                  <AddUser />
-                </Route>
-                <Route path="/">
-                  <Dashboard />
-                </Route>
-              </Switch>
-            </Wrapper>
-          </UsersProvider>
+          <Wrapper>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to="/group" />
+              </Route>
+              <Route path="/group/:id?">
+                <Dashboard />
+              </Route>
+            </Switch>
+          </Wrapper>
         </MainTemplate>
       </ThemeProvider>
     </Router>
